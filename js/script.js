4cda498f2bee1c6a61c08bff08676da44f102110
sergio <serj.sanch@gmail.com>
2018-11-02 01:19:18 +0000

    day three

--- js/script.js
+++ js/script.js
@@ -24,72 +24,75 @@ console.log(randomNumber3);
 
 console.log(randomNumber4);
 
-var numbersCorrect;
+let numbersCorrect = 0;
+
+let numbersInplace = 0;
 
 $("#verify").click(function(){
     console.log("click");
-    let input1 = $("#number1").val();
-    let input2 = $("#number2").val();
-    let input3= $("#number3").val();
-    let input4 = $("#number4").val();
+    let numberGuess1 = $("#number1").val();
+    let numberGuess2 = $("#number2").val();
+    let numberGuess3 = $("#number3").val();
+    let numberGuess4 = $("#number4").val();
     
-    $("#userChoice").text(input1 + input2 + input3 + input4);
+    $("#userChoice").text(numberGuess1 + numberGuess2 + numberGuess3 + numberGuess4);
     
-    if(input1 === randomNumber1 && input2 === randomNumber2 && input3 === randomNumber3 && input4 === randomNumber4){
+    if (numberGuess1 === randomNumber1){
+        $("#number1").css("background-color","green");
+        numbersCorrect = numbersCorrect + 1;
+        console.log(numbersCorrect);
+    }else if(numberGuess1 != randomNumber1 && numberGuess1 === (randomNumber2 || randomNumber3 || randomNumber4)){
+        $("#number1").css("background-color","orange");
+        numbersInplace = numbersInplace + 1;
+    }else if(numberGuess1 != randomNumber1){
+        $("#number1").css("background-color","red");
+        numbersCorrect = numbersCorrect + 0;
+        console.log(numbersCorrect);
+    }
+    
+    if(numberGuess2 === randomNumber2){
+        $("#number2").css("background-color","green");
+        numbersCorrect = numbersCorrect + 1;
+        console.log(numbersCorrect);
+    }else if(numberGuess2 != randomNumber2 && numberGuess2 === (randomNumber1 || randomNumber3 || randomNumber4)){
+        $("#number2").css("background-color","orange");    
+    }else if(numberGuess2 != randomNumber2){
+        $("#number2").css("background-color","red");
+        numbersCorrect = numbersCorrect + 0;
+        console.log(numbersCorrect);
+    }
+    
+    if(numberGuess3 === randomNumber3){
+        $("#number3").css("background-color","green");
+        numbersCorrect = numbersCorrect + 1;
+        console.log(numbersCorrect);
+    }else if(numberGuess3 != randomNumber3 && numberGuess3 === (randomNumber1 || randomNumber2 || randomNumber4)){
+        $("#number3").css("background-color","orange");
+    }else if(numberGuess3 != randomNumber3){
+       $("#number3").css("background-color","red"); 
+       numbersCorrect = numbersCorrect + 0;
+       console.log(numbersCorrect);
+    }
+    
+    if(numberGuess4 === randomNumber4){
+        $("#number4").css("background-color","green");
+        numbersCorrect = numbersCorrect + 1;
+        console.log(numbersCorrect);
+    }else if(numberGuess4 != randomNumber4 && numberGuess4 === (randomNumber1 || randomNumber2 || randomNumber3)){
+        $("#number4").css("background-color","orange");
+    }else if(numberGuess4 != randomNumber4){
+        $("#number4").css("background-color","red");
+        numbersCorrect = numbersCorrect + 0;
+        console.log(numbersCorrect);
+    }
+    
+    if(numbersCorrect === 4){
         $(".safe").attr("src","https://i5.walmartimages.com/asr/07d320d8-cee3-47f9-ad51-c2489c45c95e_3.bdfa1be40700c15b777918d68fbf1e60.jpeg?odnWidth=undefined&odnHeight=undefined&odnBg=ffffff");
         $("#results").text("Unlocked");
     }else{
         $("#results").text("Locked");    
     }
     
-    if (input1 === randomNumber2 || input1 === randomNumber3 ||  input1 === randomNumber4){
-        console.log("test1"); 
-        $("#number1").css("background-color","orange");
-    }else if (input1 != randomNumber1){
-        $("#number1").css("background-color","red");
-    }
-    
-    if(input2 === randomNumber1 || input2 === randomNumber3 || input2 === randomNumber4){
-        console.log("test2");
-        $("#number2").css("background-color","orange");
-    }else if(input2 !== randomNumber2){
-        $("#number2").css("background-color","red");
-    }
-    
-    if(input3 === randomNumber1 || input3 === randomNumber2 || input3 === randomNumber4){
-        console.log("test3"); 
-        $("#number3").css("background-color","orange");
-    }else if(input3 !== randomNumber3){
-        $("#number3").css("background-color","red");
-    }
-    
-    if(input4 === randomNumber1 || input4 === randomNumber2 || input4 === randomNumber3){
-        console.log("test4");
-        $("#number4").css("background-color","orange");
-    }else if(input4 !== randomNumber4){
-        $("#number4").css("background-color","red");
-    }
-
-    
-    /*
-    if(input1 === randomNumber1){
-        numbersCorrect = numbersCorrect + 1;
-        console.log(numbersCorrect);
-    }
-    
-    if(input2 === randomNumber2){
-        numbersCorrect = numbersCorrect + 1;
-        console.log(numbersCorrect);
-    }
-    
-    if(input3 === randomNumber3){
-        numbersCorrect = numbersCorrect + 1;
-        console.log(numbersCorrect);
-    }
-    
-    if(input4 === randomNumber4){
-        numbersCorrect = numbersCorrect + 1;
-        console.log(numbersCorrect);
-    }
-    */
+    numbersCorrect = 0;
+    console.log(numbersCorrect);
 });
